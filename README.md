d5f410321c66dee079aff1b3daaeebfe47869ed2
Ivan Ruano <ruano1983@gmail.com>
2024-12-31 00:42:04 +0100

    Many improvements in the script

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-This is a bash script to build qtile wm for wayland and install it in virtualenv.
+# Build qtile
+This is a bash script to build qtile wm for x11 or wayland and install it in virtualenv.
+example: 
+`chmod +x build_qtile.sh`
+for wayland:
+`./build_qtile.sh -f qtile_env -b wayland`
+for x11:
+`./build_qtile.sh -f qtile_env -b x11`
--- build_qtile.sh
+++ build_qtile.sh
@@ -5,50 +5,82 @@ YELLOW='\e[33m'
 PURPLE='\e[35m'
 NORMAL='\e[0m'
 BLUE='\e[34m'
-WELCOME="Welcome, this bash script will step by step compile and install (QTILE WM) with support for (Wayland) into your personal user folder (Home) and copy the (qtile) executable to (~/.local/bin)!"
+WELCOME="Welcome,this bash script will step by step compile and install QTILE WM\n\
+with Wayland or X11 support into your home user folder and copy the qtile executable to ~/.local/bin!"
+FOLDERQTILE="qtile_env"
 
+# list opts
+function usage {
+    printf "Usage:\n"
+    printf " -h				Display this help message.\n"
+    printf " -f <folder env>		example: qtile_env .\n"
+    printf " -b <backend>			x11 / wayland.\n"
+    exit 0
+}
+
+# opts
+while getopts hf:b: opt
+do
+    case "${opt}" in
+	 h) usage 
+	   ;;
+	 f) FOLDERQTILE=${OPTARG}
+	   ;;
+         b) if [ ${OPTARG} = "wayland" ] ; then BACKEND="wayland" ; else BACKEND="x11" ; fi
+	   ;;
+	 ?) echo "script usage [-f folder env] [-b backend] [-h]" >&2
+	   exit 1
+	   ;;
+    esac
+done
+
+if [ -z  "$BACKEND" ]; then
+    echo "You have not introduced the backend argument"
+    exit 1
+fi
 echo -e "${BLUE}${WELCOME}${NORMAL}"
-echo -n -e "${YELLOW}Do you want to continue with the installation? : (y/n)${NORMAL} "
-read confirmation
+echo -n -e "${YELLOW}Do you want to continue with the installation ? [y/n*]\n${NORMAL}"
+read -p "> " confirmation
 if [ $confirmation = "y" ]
-then
-    cd ~ 	
-    echo -n -e "${YELLOW}Enter the folder where to install qtile as virtualenv : ${NORMAL} "
-    read FOLDERENV
-    echo -n -e "${YELLOW}will be installed in this folder '$HOME/${FOLDERENV}'${NORMAL} "
-    echo -n -e "${YELLOW}You're sure? : (y/n)${NORMAL} "
-    read confirmation
-    if [ $confirmation = "y" ]
-    then
-	python3 -m venv ${FOLDERENV}
-	cd  ~/${FOLDERENV}
+then	
+	echo -e "${GREEN}starting installation...${NORMAL}"
+	sleep 1
+	cd ~
+	python3 -m venv ${FOLDERQTILE}
+	cd  ~/${FOLDERQTILE}
 	git clone https://github.com/qtile/qtile.git
-	git clone https://github.com/flacjacket/pywayland.git
-	git clone https://github.com/flacjacket/pywlroots.git
-	git clone https://github.com/sde1000/python-xkbcommon.git	
-	# install pywayland and pywlroots 
-	# build qtile and pywlroots with wlroots 0.17
-	export CFLAGS="$CFLAGS -I/usr/include/wlroots0.17"
-	export LDFLAGS="$LDFLAGS -L/usr/lib/wlroots0.17"
-	bin/pip3 install xcffib
-	bin/pip3 install python-xkbcommon/.
-	bin/pip3 install pywayland/.
-	bin/pip3 install pywlroots/.
-	source bin/activate
-	cd ~/${FOLDERENV}/qtile
-	# install Qtile
-	pip3 install --config-setting backend=wayland . 
-	cd ~/${FOLDERENV}
-	mkdir -p  ~/.local/bin
+	case ${BACKEND} in
+	    x11)		
+	    # X11 Backend
+	    bin/pip3 install xcffib
+	    source bin/activate
+	    cd ~/${FOLDERQTILE}/qtile
+	    # install qtile x11 backend
+	    pip3 install . 
+	    ;;
+	    wayland)
+	    # Wayland Backend
+	    git clone https://github.com/flacjacket/pywayland.git
+	    git clone https://github.com/flacjacket/pywlroots.git
+	    git clone https://github.com/sde1000/python-xkbcommon.git	
+	    # install pywayland and pywlroots 
+	    # build qtile and pywlroots with wlroots 0.17
+	    export CFLAGS="$CFLAGS -I/usr/include/wlroots0.17"
+	    export LDFLAGS="$LDFLAGS -L/usr/lib/wlroots0.17"
+	    bin/pip3 install python-xkbcommon/.
+	    bin/pip3 install pywayland/.
+	    bin/pip3 install pywlroots/.
+	    source bin/activate
+	    cd ~/${FOLDERQTILE}/qtile
+	    # install qtile wayland backend
+	    pip3 install --config-setting backend=wayland . 
+	    ;;
+	esac
+	cd ~/${FOLDERQTILE}
 	cp bin/qtile ~/.local/bin
 	pip3 install psutil
-	pip3 install dbus-next
+	pip3 install dbus-fast
 	pip3 install pulsectl-asyncio
 	pip3 install iwlib
- 	echo -e "${GREEN}installation completed :)${NORMAL}"
-    else  
- 	echo -e "${RED}installation cancelled${NORMAL}"
-    fi    
-else
-    echo -e "${RED}installation cancelled${NORMAL}"	    
+	echo -e "${GREEN}Installation completed :)${NORMAL}"
 fi
